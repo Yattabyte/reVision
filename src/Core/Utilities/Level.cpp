7bf4e6754e889bd8c46c05238cbabdb0b35b9a2d
Troy Konrad Lowry <tklowry93@gmail.com>
2017-11-08 16:02:31 -0600

    Version: 0.15
    Added preliminary level namespace with a static vector of entities

---
+++ src/Core/Utilities/Level.cpp
@@ -0,0 +1,13 @@
+#include "Utilities\Level.h"
+#include "Entities\Entity.h"
+#include <vector>
+
+using namespace std;
+
+static vector<Entity> level_entities;
+
+namespace Level {
+
+
+}
+
---
+++ src/Core/Utilities/Level.h
@@ -0,0 +1,20 @@
+/*
+	Level
+
+	- A primitive static container that holds a list of level entities
+*/
+
+#pragma once
+#ifndef LEVEL
+#define LEVEL
+#ifdef	DT_CORE_EXPORT
+#define DELTA_CORE_API __declspec(dllexport)
+#else
+#define	DELTA_CORE_API __declspec(dllimport)
+#endif
+
+namespace LEVEL {
+
+}
+
+#endif // LEVEL
