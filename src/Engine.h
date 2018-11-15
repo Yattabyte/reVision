eb5dc4a2c04b74246d0b56a89d733e77d3d1d728
Troy Lowry <tklowry93@gmail.com>
2018-11-15 11:16:53 -0600

    Version: 2.3.I
    Modified board models and tile texture

--- src/Engine.h
+++ src/Engine.h
@@ -18,7 +18,7 @@
 #include <string>
 
 
-constexpr char ENGINE_VERSION[] = "2.3.H";
+constexpr char ENGINE_VERSION[] = "2.3.I";
 constexpr int DESIRED_OGL_VER_MAJOR = 4;
 constexpr int DESIRED_OGL_VER_MINOR = 5;
 
