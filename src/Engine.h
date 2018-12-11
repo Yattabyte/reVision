6322dbb0001fc8b03c4c356a04d7bc19332c9777
Troy Lowry <tklowry93@gmail.com>
2018-12-11 13:36:49 -0600

    Version: 2.7.B
    - Added source information for the sound files used.

--- src/Engine.h
+++ src/Engine.h
@@ -20,7 +20,7 @@
 #include <string>
 
 
-constexpr char ENGINE_VERSION[] = "2.7.A";
+constexpr char ENGINE_VERSION[] = "2.7.B";
 constexpr int DESIRED_OGL_VER_MAJOR = 4;
 constexpr int DESIRED_OGL_VER_MINOR = 5;
 
